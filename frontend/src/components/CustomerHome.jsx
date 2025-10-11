f452e7e027a12538870311ed1312d97e575726e6
carmelo-gulino <s346140@studenti.polito.it>
2025-10-11 16:58:29 +0200

    Refactor CustomerHome, dynamic service selection

--- frontend/src/components/CustomerHome.jsx
+++ frontend/src/components/CustomerHome.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react'
 import { Dropdown } from 'react-bootstrap'
 
 function CustomerHome() {
+    const [selectedService, setSelectedService] = useState(null);
+
+    const mockServices = [
+        { id: "S1", name: "Service 1" },
+        { id: "S2", name: "Service 2" },
+        { id: "S3", name: "Service 3" },
+        { id: null, name: "None" }
+    ];
+    
+    const handleSelect = (eventKey) => {
+        setSelectedService(eventKey);
+    }
+
     return (
         <>
-        <Dropdown>
-            <Dropdown.Toggle variant='dark' id="dropdown-basic">
-                Select a service
+        <Dropdown onSelect={handleSelect}>
+            <Dropdown.Toggle variant='success' id="dropdown-basic">
+                {selectedService 
+                    ? mockServices.find(s => s.id === selectedService).name
+                    : "Select a service"
+                }
             </Dropdown.Toggle>
 
-            <Dropdown.Menu>
-                <Dropdown.Item>Service 1</Dropdown.Item>
-                <Dropdown.Item>Service 2</Dropdown.Item>
-                <Dropdown.Item>Service 3</Dropdown.Item>
-                <Dropdown.Item>None</Dropdown.Item>
+            <Dropdown.Menu>     {/**il valore deve essere preso dal backend e messo come value */}
+                {mockServices.map(s => (
+                    <Dropdown.Item key={s.id} eventKey={s.id}>{s.name}</Dropdown.Item>
+                ))}
             </Dropdown.Menu>
         </Dropdown>
         </>
