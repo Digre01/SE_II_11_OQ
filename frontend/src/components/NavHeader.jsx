fe8eba0a547629ec3fc885d6680d444533da0db4
Digre01 <79282925+Digre01@users.noreply.github.com>
2025-10-12 11:14:57 +0200

    home button

--- frontend/src/components/NavHeader.jsx
+++ frontend/src/components/NavHeader.jsx
@@ -1,11 +1,25 @@
-import { Navbar } from "react-bootstrap";
+import { Navbar, Button} from "react-bootstrap";
+import { useNavigate } from 'react-router';
 
 function NavHeader() {
+    const navigate = useNavigate();
 
     return(
         <Navbar expand="lg" bg="info" className="d-flex justify-content-center align-items-center">
-            <h1 className="fw-bold display-4 text-center">Office Queue Management System</h1>
+            
+        <Button
+            variant="light"
+            className="position-absolute end-0 me-3"
+            onClick={() => navigate('/')}
+            >
+            Home
+        </Button>
+        
+        <h1 className="fw-bold display-4 text-center">Office Queue Management System</h1>
+    
+        
         </Navbar>
+        
     )
 }
 
